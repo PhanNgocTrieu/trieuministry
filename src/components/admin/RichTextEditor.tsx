f6e985f7c1227dd97dce1f84562c50ac80f24428
tpn <m1@MacBook-Pro-cua-M1.local>
2026-01-28 22:51:25 +0700

    feat: add color and background options to RichTextEditor

--- src/components/admin/RichTextEditor.tsx
+++ src/components/admin/RichTextEditor.tsx
@@ -24,6 +24,7 @@ const modules = {
     toolbar: [
         [{ 'header': [1, 2, 3, false] }],
         ['bold', 'italic', 'underline', 'strike'],
+        [{ 'color': [] }, { 'background': [] }],
         [{ 'list': 'ordered'}, { 'list': 'bullet' }],
         ['link', 'blockquote'],
         ['clean']
